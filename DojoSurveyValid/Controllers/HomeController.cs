2c2659b5c511682480a1c3cf2a19081fba4bc2ba
Kevin <stradtkt22@gmail.com>
2018-08-12 10:39:15 -0400

    Added Unfinished DojoSurveyValid

---
+++ DojoSurveyValid/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurveyValid.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace DojoSurveyValid.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index(int id)
+        {
+            ViewData["Message"] = "Send Some Info Through This Form";
+            var model = new FormData();
+            model.Location = "Seattle";
+            model.Location = "Washington D.C.";
+            model.Location = "Redmond W.A.";
+            model.Location = "Austin Texas";
+            model.Language = "PHP";
+            model.Language = "C#";
+            model.Language = "C++";
+            model.Language = "JavaScript";
+            return View(model);
+        }
+        [HttpPost("result")]
+        public IActionResult ResultData(FormData data)
+        {
+            ViewData["Message"] = "Data From The Form";
+            if(ModelState.IsValid)
+            {
+                return View(data);
+            }
+            return View("Index");
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DojoSurveyValid/Models/FormData.cs
@@ -0,0 +1,41 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+namespace DojoSurveyValid.Models
+{
+    public class FormData
+    {
+        [Required]
+        [MinLength(2, ErrorMessage="You need to have at least 2 characters for the name")]
+        [MaxLength(20, ErrorMessage="You cannot exceed 20 characters for the name")]
+        [Display(Name="Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Location")]
+        public string Location { get; set; }
+        public List<SelectListItem> Locations {get;} = new List<SelectListItem>()
+        {
+            new SelectListItem {Value="Seattle", Text="Seattle"},
+            new SelectListItem {Value="Washington D.C.", Text="Washington D.C."},
+            new SelectListItem {Value="Redmond W.A.", Text="Redmond W.A."},
+            new SelectListItem {Value="Austin Texas", Text="Austin Texas"}
+        };
+
+        [Required]
+        [Display(Name="Favorite Language")]
+        public string Language { get; set; }
+        public List<SelectListItem> Languages {get;} = new List<SelectListItem>()
+        {
+            new SelectListItem {Value="PHP", Text="PHP"},
+            new SelectListItem {Value="C#", Text="C#"},
+            new SelectListItem {Value="C++", Text="C++"},
+            new SelectListItem {Value="JavaScript", Text="JavaScript"}
+        };
+        
+        [MinLength(5, ErrorMessage="You must have at least 5 characters for the comment")]
+        [MaxLength(100, ErrorMessage="You cannot exceed 100 characters for the comment")]
+        [Display(Name="Comment")]
+        public string Comment { get; set; }
+    }
+}
